a1cb2d9c1ac210eb816199a54d226ebce3fb325b
pmelo-ca <pedromelocf@yahoo.com>
2024-02-01 19:04:47 -0300

    Creating trashman

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:29:58 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/01 18:50:07 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/01 19:04:29 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@
 # include "../lib/gnl/get_next_line.h"
 # include "../lib/libft.h"
 # include "../lib/printf/ft_printf.h"
+
+typedef struct s_trashman
+{
+	
+}	t_trashman;
 
 #endif
--- push_swap_utils/main.c
+++ push_swap_utils/main.c
@@ -6,14 +6,19 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:53:46 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/01 18:56:08 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/01 19:01:51 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int main (void)
+int	main(void)
 {
 	ft_printf("Test OK :) \n");
 	return (0);
+}
+
+void	*trashman(void *arg, t_trashman *trashman)
+{
+	
 }
