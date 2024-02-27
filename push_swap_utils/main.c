6de9ea2b09b13c4393542e3e1e12d0a175da62c2
Pedro Melo <pedromelocf@yahoo.com>
2024-02-27 20:41:23 -0300

    Renaming function

--- push_swap_utils/main.c
+++ push_swap_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:53:46 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/26 18:46:51 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/27 20:32:38 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	btree = validate_arg(argc, argv);
 	if (!btree)
 		return (ft_dprintf(2, "Error\n"));
-	get_pos(btree);
+	get_index(btree);
 	init_stacks(stack_a, stack_b, btree);
 	clean_tree(btree);
 	return (0);
