2c0154ff0b394a460a6d55f4c247af2c64deacc7
Pedro Melo <pedromelocf@yahoo.com>
2024-02-26 21:07:10 -0300

    Inserting init stack function and get pos function. Still need to create the function

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:29:58 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/26 13:09:00 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/26 18:46:52 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,7 @@ t_btree				*validate_arg(int argc, char **argv);
 int					*get_int_argc(int argc, char **argv);
 t_btree				*new_tree(int argc, int *stack_a);
 t_btree				*insert_node(t_btree *head, int value, int counter);
+void				get_pos(t_btree *btree);
+void				init_stacks(t_stack *stack_a, t_stack *stack_b, t_btree *btree);
 void				clean_tree(t_btree *btree);
 #endif
