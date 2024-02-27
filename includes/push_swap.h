67d6eec52bb87b88b1c8bdde816d426b5c3c998a
Pedro Melo <pedromelocf@yahoo.com>
2024-02-27 20:31:08 -0300

    Fixing data structure

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:29:58 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/27 20:20:07 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/27 20:30:56 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 
 typedef struct s_btree
 {
+	int				value;
 	int				index;
 	int				pos;
 	struct s_btree	*right;
