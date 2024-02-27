d57e5548c69edaed1f5bab53a6435650fcacde82
Pedro Melo <pedromelocf@yahoo.com>
2024-02-27 20:25:10 -0300

    Fixing data structure

--- push_swap_utils/btree_utils.c
+++ push_swap_utils/btree_utils.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 16:00:43 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/02/26 13:13:17 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/02/27 20:23:03 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,8 @@ t_btree	*insert_node(t_btree *btree, int arg, int counter)
 		tree_node = malloc(sizeof(t_btree));
 		if (!tree_node)
 			return (NULL);
-		tree_node->value = arg;
-		tree_node->index = counter;
-		tree_node->pos = 0;
+		tree_node->index = 0;
+		tree_node->pos = counter;
 		tree_node->left = NULL;
 		tree_node->right = NULL;
 		return (tree_node);
