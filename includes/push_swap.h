91366f0e401a57aee57c0991beb92b55df7ea083
Pedro Melo <pedromelocf@yahoo.com>
2024-03-21 11:39:49 -0300

    Creating branch for testing my data structure. Array of int, tree creation and tree index are being tested.

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:29:58 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/03/21 00:02:32 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:39:03 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,4 +54,8 @@ void				inorder_transversal(t_btree **btree, int *index);
 void				init_stacks(t_stack **stack_a, t_stack **stack_b, t_btree **btree, int *arr_int);
 int					search_index(int value, t_btree *btree);
 void				clean_tree(t_btree *btree);
+//					TESTING
+void 				print_arr_int(int *arr_int, int lenght);
+void 				print_tree(t_btree *btree);
+void 				print_tree_index(t_btree *btree);
 #endif
--- push_swap_utils/main.c
+++ push_swap_utils/main.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:53:46 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/03/18 14:17:09 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:38:50 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,44 @@ int	main(int argc, char **argv)
 	t_stack *stack_b;
 
 	validate_arg(argc, argv, &arr_int);
+	print_arr_int(arr_int, argc);
 	new_tree(argc, arr_int, &btree);
+	print_tree(btree);
+	printf("\n");
 	inorder_transversal(&btree, &btree->index);
+	print_tree_index(btree);
+	printf("\n");
 	init_stacks(&stack_a, &stack_b, &btree, arr_int);
 	return (0);
+}
+
+void print_arr_int(int *arr_int, int lenght)
+{
+	int i;
+
+	i = 1;
+	while (i < lenght)
+	{
+		printf("Element %d from array_int:%d\n", i, arr_int[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+void print_tree(t_btree *btree)
+{
+	if (btree->left != NULL)
+		print_tree(btree->left);
+	printf("Tree node value: %d\n", btree->value);
+	if (btree->right != NULL)
+		print_tree(btree->right);
+}
+
+void print_tree_index(t_btree *btree)
+{
+	if (btree->left != NULL)
+		print_tree_index(btree->left);
+	printf("Tree node value: %d, Tree node index: %d\n", btree->value, btree->index);
+	if (btree->right != NULL)
+		print_tree_index(btree->right);
 }
