c6256a244d537f83c4e809b9ccfe2f3b729a3db6
Pedro Melo <pedromelocf@yahoo.com>
2024-03-29 00:28:43 -0300

    inserting push and swap operations. Already tested but still need more testing

--- push_swap_utils/operations.c
+++ push_swap_utils/operations.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:43:08 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/03/26 17:33:07 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/03/29 00:27:40 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,37 @@
 
 void	push(t_stack **stack_source, t_stack **stack_dest)
 {
+	t_node *temp;
 
+	if(!(*stack_source))
+		return;
+	temp = dup_node((*stack_source)->top);
+	(*stack_source)->top = (*stack_source)->top->prev;
+	if ((*stack_dest)->top == NULL)
+		temp->prev = NULL;
+	else
+		temp->prev = (*stack_dest)->top;
+	temp->next = NULL;
+	(*stack_dest)->top = temp;
 }
 
 void	swap(t_stack **stack_source)
 {
+	t_node *temp;
 
+	if(!(*stack_source))
+		return;
+	if((*stack_source)->top->prev == NULL)
+		return;
+	temp = dup_node((*stack_source)->top);
+	if(temp == NULL)
+		exit_status(6, NULL);
+	(*stack_source)->top = (*stack_source)->top->prev;
+	(*stack_source)->top->next = NULL;
+	(*stack_source)->top->prev->next = temp;
+	temp->prev = (*stack_source)->top->prev;
+	temp->next = (*stack_source)->top;
+	(*stack_source)->top->prev = temp;
 }
 
 void	rotate(t_stack **stack_source)
