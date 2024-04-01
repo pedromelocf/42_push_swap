c944719371e71460b0a14fa0f2510f26fed1f7c9
pmelo-ca <pedromelocf@yahoo.com>
2024-04-01 19:19:45 -0300

    Inserting clean_stacks function

--- push_swap_utils/init_stacks.c
+++ push_swap_utils/init_stacks.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 16:25:55 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/04/01 16:59:01 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/04/01 18:37:26 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,3 +84,7 @@ void	push_top(t_stack **stack, int index, int pos, int value)
 	}
 }
 
+void clean_stacks(t_stack **stack_a, t_stack **stack_b)
+{
+	
+}
