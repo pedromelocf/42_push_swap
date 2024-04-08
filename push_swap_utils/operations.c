669b0f6f64312ba05fdf2e06f3f4b5adb9f32360
pmelo-ca <pedromelocf@yahoo.com>
2024-04-08 12:37:24 -0300

    Fixing mem leaks. Not working 100% yet. Need fix push and rotate

--- push_swap_utils/operations.c
+++ push_swap_utils/operations.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:43:08 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/04/05 17:15:41 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/04/08 12:36:07 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void	swap(t_stack **stack_source, char *message)
 		return ;
 	if ((*stack_source)->top->prev == NULL)
 		return ;
-	temp = dup_node((*stack_source)->top);
+	temp = (*stack_source)->top;
 	if (temp == NULL)
 		exit_status(6, NULL);
 	(*stack_source)->top = (*stack_source)->top->prev;
@@ -84,7 +84,7 @@ void	reverse_rotate(t_stack **stack_source, char *message)
 
 	while ((*stack_source)->top->prev != NULL)
 		(*stack_source)->top = (*stack_source)->top->prev;
-	temp = dup_node((*stack_source)->top);
+	temp = (*stack_source)->top;
 	if (temp == NULL)
 		exit_status(6, NULL);
 	(*stack_source)->top = (*stack_source)->top->next;
