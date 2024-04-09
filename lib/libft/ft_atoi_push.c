2acd72501421ab5d995fe7207bf199419289f1af
pmelo-ca <pedromelocf@yahoo.com>
2024-04-09 15:20:34 -0300

    fixing input validation

--- lib/libft/ft_atoi_push.c
+++ lib/libft/ft_atoi_push.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 21:36:34 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2024/04/05 17:17:33 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:19:44 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ int	ft_atoi_push(const char *nptr, int *error)
 	i = 0;
 	sign = 1;
 	*error = 0;
-	while ((nptr[i] == ' ') || (nptr[i] >= '\t' && nptr[i] <= '\r'))
-		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
 			sign *= -1;
 		i++;
 	}
+	if (nptr[i] < '0' || nptr[i] > '9')
+		*error = 1;
 	while ((nptr[i] >= '0') && (nptr[i] <= '9'))
 	{
 		c = c * 10 + nptr[i] - '0';
 		i++;
 	}
-	if (nptr[i] != '\0' || c > INT_MAX)
+	if (nptr[i] != '\0' || c > INT_MAX || c < INT_MIN)
 		*error = 1;
 	return (sign * c);
 }
